e9c93f833695a07426c2919e872086da7adce6bb
Biswajit Biswal <biswajitbiswal.in@gmail.com>
2026-08-10 22:09:15 +0530

    I/O In java

---
+++ Basics_of_java/main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        System.out.println("Hello world");
+         System.out.print("Hello world");
+
+         String name = "Biswa";
+         int a = 56;
+         int b = 56;
+
+         int sum = a + b;
+         System.out.println(sum);
+         int mul = a*b;
+         System.out.println(mul);
+
+         //input
+         Scanner sc = new Scanner(System.in);
+        //  String user = sc.next();
+        //nextInt()
+        //nextFloat()
+        //nextDouble()
+             String user = sc.nextLine();
+         System.out.println(user);
+    }
+}
